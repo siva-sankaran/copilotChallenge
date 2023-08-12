2e18e0d1e25b7272f8500d49c29dbf7e38b249a4
Siva Sankaran S <7930913+siva-sankaran@users.noreply.github.com>
2023-08-12 12:10:49 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,40 @@
+// create function to add two integers
+function add(x, y) {
+  return x + y;
+}
+
+//create function to check whether the give number is prime or not
+function isPrime(n) {    
+  if (n <= 1) {
+    return false;
+  }
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0) return false;
+  }
+  return true;
+}
+
+//create web server
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello World");
+    res.end();
+  }
+  if (req.url === "/api/numbers/add") {
+    const url_parts = url.parse(req.url, true);
+    const query = url_parts.query;
+    const x = parseInt(query.x);
+    const y = parseInt(query.y);
+    const sum = x + y;
+    res.write(`Sum of ${x} and ${y} is ${sum}`);
+    res.end();
+  }
+  if (req.url === "/api/numbers/prime") {
+    const url_parts = url.parse(req.url, true);
+    const query = url_parts.query;
+    const n = parseInt(query.number);
+    const prime = isPrime(n);
+    res.write(`${n} is prime number: ${prime}`);
+    res.end();
+  }
+});
